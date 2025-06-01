4cbe3e3157559771a3db681a890d62f1afc43253
byeonheejae <pion0458@gmail.com>
2025-06-02 00:22:09 +0900

    기존 1팀 프로젝트

---
+++ src/test/java/com/codeit/duckhu/DuckhuApplicationTests.java
@@ -0,0 +1,11 @@
+package com.codeit.duckhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckhuApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
